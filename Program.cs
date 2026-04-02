7744303f49c462b2b24ac9f826ae66ffacee28ff
6lood1ess <6lo.od1ess@gmail.com>
2026-04-02 14:54:36 +0700

    - Add main program entry point with directory and single file processing mode selection

--- Program.cs
+++ Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace WordCorrector
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace WordCorrector {
+  class Program {
+    static void Main() {
+
+      Console.WriteLine("-=*= Text error corrector =*=-\n");
+
+      DictionaryService dictionaryService = new DictionaryService();
+      TextCorrector textCorrector = new TextCorrector(dictionaryService);
+      FileProcessor fileProcessor = new FileProcessor(textCorrector);
+
+      Console.WriteLine("Select mode:\n" +
+                        "1 - Process all .txt files in directory\n" +
+                        "2 - Process a single file\n");
+
+      Console.Write("Your choice: ");
+      string userChoice = Console.ReadLine();
+
+      if (userChoice == "1") {
+
+        Console.Write("\nEnter directory path: ");
+        string directory = Console.ReadLine();
+                
+        if (Directory.Exists(directory)) {
+          fileProcessor.ProcessAllTxtFiles(directory);
+        } else {
+          Console.WriteLine("Directory not found!");
         }
+      } else if (userChoice == "2") {
+
+        Console.Write("\nEnter file path: ");
+        string filePath = Console.ReadLine();
+                
+        if (File.Exists(filePath)) {
+          fileProcessor.ProcessSingleFile(filePath);
+        } else {
+          Console.WriteLine("File not found!");
+        }
+      } else {
+        Console.WriteLine("\nInvalid choice!");
+      }
+
+      Console.WriteLine("\nPress any key to exit...");
+      Console.ReadKey();
     }
+  }
 }
